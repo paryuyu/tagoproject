4245959f5c249d86ed72706c838138cbd125ab16
유유유 <107971288+paryuyu@users.noreply.github.com>
2023-02-15 16:49:36 +0900

    Update tagoAPI.js

--- src/util/tagoAPI.js
+++ src/util/tagoAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let key ="Lg3xW55zPXZOKKGy%2F9yuUYPbKZnBYzrDHtABcdAECRZiwNt0igZqOXoADrNI7%2BoSNrgN6raYgbrkNG2%2FUWerGA%3D%3D";
+let key =process.env.REACT_APP_KEY;
 let endpoint = process.env.REACT_APP_END_POINT;
 let baseUrl = process.env.REACT_APP_BASEURL;
 
